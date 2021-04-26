27f05cc7a9187a730f12af46c56314e73fa5c114
yulia <yulka84@gmail.com>
2021-04-26 14:31:01 +0300

    todolist+todoitem styles

--- src/components/toDoItem/toDoItem.js
+++ src/components/toDoItem/toDoItem.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './toDoItem.css'
 
 export default function ToDoItem({ item, onChange }) {
     let className = item.isCompleted ? 'completed' : '';
@@ -8,12 +9,11 @@ export default function ToDoItem({ item, onChange }) {
     function checkChanged(item) {
         setChecked(!checked);
         return onChange(item);
-        console.log("checkChanged", item); 
     }
     return (
-        <div>
-            <input type="checkbox" checked={checked} onChange={() => checkChanged(item)}></input>
-            <label name="todo" className={className}>{item.text}</label>
+        <div className="item">
+            <input name="todo" type="checkbox" checked={checked} onChange={() => checkChanged(item)}></input>
+            <label for="todo" className={className}>{item.text}</label>
         </div>
     )
 }
--- src/components/toDoList/toDoList.js
+++ src/components/toDoList/toDoList.js
@@ -1,11 +1,12 @@
 import ToDoItem from '../toDoItem/toDoItem';
+import './toDoList.css'
 
 export default function ToDoList({ list, onChange }) {
     let todoList = list.map(item => {
         return <ToDoItem item={item} onChange={e => onChange(e)}></ToDoItem>
     })
     return (
-        <div>
+        <div className="list">
             {todoList}
         </div>
     )
