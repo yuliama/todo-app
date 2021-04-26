2e5c3c4065c14ba891fb5953f7bf1973de0db67d
yulia <yulka84@gmail.com>
2021-04-26 10:04:05 +0300

    addnewtodo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header'
+import AddNewTodo from './components/addNewTodo/addNewTodo'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <AddNewTodo></AddNewTodo>
     </div>
   );
 }
---
+++ src/components/addNewTodo/addNewTodo.js
@@ -0,0 +1,9 @@
+import './addNewTodo.css';
+
+export default function AddNewTodo() {
+    return (
+        <div className="addNewTodo">
+            <input type="text" placeholder="What's next?"></input>
+        </div>
+    )
+}
