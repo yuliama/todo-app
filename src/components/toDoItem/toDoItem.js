134fbc00c35c9940db2f7925a2c410bcbb00890c
yulia <yulka84@gmail.com>
2021-04-27 20:10:29 +0300

    fix checkboxes bug

--- src/components/toDoItem/toDoItem.js
+++ src/components/toDoItem/toDoItem.js
@@ -2,17 +2,9 @@ import { useState } from "react";
 import './toDoItem.css'
 
 export default function ToDoItem({ item, onChange, onDelete }) {
-    const [checked, setChecked] = useState(item.isCompleted);
     const [showDeleteIcon, setshowDeleteIcon] = useState('hide');
 
     let deleteIcon = <input className={showDeleteIcon + ' deleteIcon'} onClick={() => onDelete(item)} type="button" ></input >
-    let className = item.isCompleted ? 'completed' : '';
-    let itemId = 'todo' + item.id;
-
-    function checkChanged(item) {
-        setChecked(!checked);
-        return onChange(item);
-    }
 
     function addDeleteIcon(showIcon) {
         showIcon ? setshowDeleteIcon('show') : setshowDeleteIcon('hide');
@@ -20,8 +12,8 @@ export default function ToDoItem({ item, onChange, onDelete }) {
 
     return (
         <div className="item" onMouseEnter={() => addDeleteIcon(true)} onMouseLeave={() => addDeleteIcon(false)}>
-            <input id={itemId} type="checkbox" checked={checked} onChange={() => checkChanged(item)}></input>
-            <label htmlFor={itemId} className={className}>{item.text}</label>
+            <input id={'todo' + item.id} type="checkbox" checked={item.isCompleted} onChange={() => onChange(item)}></input>
+            <label htmlFor={'todo' + item.id} className={item.isCompleted ? 'completed' : ''}>{item.text}</label>
             {deleteIcon}
         </div>
     )
